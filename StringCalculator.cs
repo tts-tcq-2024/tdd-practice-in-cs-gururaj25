ec1e1255ff09c34c9c867e0e7542d2840d6ab029
gururaj25 <51817915+gururaj25@users.noreply.github.com>
2024-07-17 19:42:32 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -6,5 +6,20 @@ public class StringCalculator
         {
             return 0;
         }
+    var separator = new List<string> { ",", "\n" };
+    // Split the input string using the separator
+        string[] splitNumbers = input.Split(separator.ToArray(), StringSplitOptions.None);
+
+        // Convert the split strings to integers
+        List<int> numbers = splitNumbers.Select(int.Parse).ToList();
+
+        var negativeNumbers = numbers.Where(n => n < 0).ToList();
+
+        if (negativeNumbers.Any())
+        {
+            throw new Exception($"Negatives not allowed: {string.Join(", ", negativeNumbers)}");
+        }
+
+        return numbers.Where(n => n <= 1000).Sum();
   }
 }
